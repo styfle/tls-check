321f756aa2d1ecbc7d379323e4de00fe6885cea4
Steven <steven@ceriously.com>
2019-11-12 20:33:37 -0500

    Update link to docs

--- methods.js
+++ methods.js
@@ -1,9 +1,7 @@
-// Taken from https://www.openssl.org/docs/man1.1.0/ssl/ssl.html#Dealing-with-Protocol-Methods
-const methods = [
+// https://nodejs.org/api/tls.html#tls_tls_default_min_version
+module.exports = [
     'TLSv1',
     'TLSv1.1',
     'TLSv1.2',
     'TLSv1.3',
 ];
-
-module.exports = methods;
